b2b7d0245ec2417d17010fdd8e2a07d05974b713
Nick Lozenski <nicholas.lozenski@gmail.com>
2018-04-06 01:12:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(`${string.toLowerCase()}`)
 }
-<<<<<<< HEAD
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return ("I can't hear you!")
@@ -20,19 +19,5 @@ function sayHiToGrandma(string) {
   }
   else {
     return ('I love you, too.')
-=======
-var uppercase = "HELLO!"
-var lowercase = 'hello!'
-var mixedcase = 'Hi there!'
-function sayHiToGrandma(string) {
-  if (lowercase.toLowerCase() === lowercase) {
-    return ("I can't hear you!")
-  }
-  else if (uppercase.toUpperCase() === uppercase) {
-    return ('YES INDEED!')
-  }
-  else {
-    return ('I love you, too!')
->>>>>>> 0fa62747f9ff6ce2c95fdf949730964bba17fb5b
   }
 }
